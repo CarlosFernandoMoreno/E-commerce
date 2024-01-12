f1be961ac9933d4a8cfe496490341d5be70a9a20
Main <gimenezfrancotomas96@gmail.com>
2024-01-11 22:20:38 -0300

    creacion de las rutas de form y buy

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const { getProductCTRL, createProductCTRL, aboutUsCTRL, contactUsCTRL, } = require('../controllers/product.controller.js')
+const { getProductCTRL, createProductCTRL, aboutUsCTRL, formCTRL, contactUsCTRL, verifyStockCTRL, } = require('../controllers/product.controller.js')
 const { handleError } = require('../middlewares/error.middleware.js')
 const { productValidator } = require('../validators/product.validator.js')
 
@@ -7,7 +7,8 @@ router.get('/', getProductCTRL)
 router.post('/loadproduct', productValidator, createProductCTRL)
 router.get('/contactus', contactUsCTRL)
 router.get('/aboutus', aboutUsCTRL)
-
+router.get('/form', formCTRL)
+router.patch('/buy', verifyStockCTRL)
 router.use(handleError)
 
 module.exports = router
