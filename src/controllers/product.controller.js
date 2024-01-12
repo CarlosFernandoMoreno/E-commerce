963871f1fe7645fff6210a39ff1decb72e66af9f
Main <gimenezfrancotomas96@gmail.com>
2024-01-11 22:19:35 -0300

    creacion de verifystockCTRL y render form

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,9 @@
-const { serviceGet } = require('../services/product.service.js')
+const { serviceGet, serviceStock, serviceSell, } = require('../services/product.service.js')
 
 const getProductCTRL = async (req, res) => {
   const product = await serviceGet()
-  
   try {
+    console.log(product)
     res.render('index', { product })
   } catch (error) {
     console.error(error)
@@ -11,8 +11,6 @@ const getProductCTRL = async (req, res) => {
     res.status(500).send('Error interno del servidor')
   }
 }
-
-
 
 const aboutUsCTRL = (req, res) => {
   try {
@@ -29,7 +27,17 @@ const contactUsCTRL = (req, res) => {
     res.render('contactUs')
   } catch (error) {
     console.error(error)
-    console.error('no se pudo renderizar la vista about us')
+    console.error('no se pudo renderizar la vista contact us')
+    res.status(500).send('Error interno del servidor')
+  }
+}
+
+const formCTRL = (req, res) => {
+  try {
+    res.render('form')
+  } catch (error) {
+    console.error(error)
+    console.error('no se pudo renderizar la vista form')
     res.status(500).send('Error interno del servidor')
   }
 }
@@ -40,6 +48,7 @@ const createProductCTRL = (req, res) => {
     title,
     description,
     price,
+    stock,
   })
   product
     .save()
@@ -49,9 +58,47 @@ const createProductCTRL = (req, res) => {
     .catch((error) => res.status(400).json('error al guardar la publicacion' + error))
 }
 
+const verifyStockCTRL = async (req, res) => {
+  try {
+    const { quantity } = req.body
+    if (!quantity || quantity <= 0 || isNaN(quantity)  ) {
+      return res.status(400).send(`Se requiere la cantidad deseada en numeros enteros y positivos.`)
+    }
+
+    const answerStock = await serviceStock(quantity)
+
+    if (answerStock.success) {
+       await serviceSell(quantity)
+      
+      res.status(200).send('venta check')
+    } else {
+      console.log(`tas en el else de verifyStock perro. ${answerStock.message}`)
+      res.status(400).send(answerStock)
+    }
+  } catch {
+    console.error(error)
+    console.error('tas en el catch de verifyStock perro')
+    res.status(500).send('Error interno del servidor.')
+  }
+}
+
+
+/*
+controler para renderizar formulario - check
+
+path check
+llamamos al service de validacion de stock check
+
+try  llamamos al service donde descontamos la cantidad de unidades de la 
+database check 
+catch errores no contemplados mas arriba 
+*/
+
 module.exports = {
   getProductCTRL,
   createProductCTRL,
   aboutUsCTRL,
   contactUsCTRL,
+  formCTRL,
+  verifyStockCTRL,
 }
